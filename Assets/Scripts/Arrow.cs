c28b81da7296fcea851696b296c95e419e0d2bfa
Zachary Rocco <zr01385@srcs.org>
2020-11-17 23:11:56 -0800

    Milestone 1 complete

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    public MeshRenderer mesh;
+    public GameObject player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 location = new Vector3(player.transform.position.x + Mathf.Sin(transform.rotation.eulerAngles.y * Mathf.PI / 180), player.transform.position.y, player.transform.position.z + Mathf.Cos(transform.rotation.eulerAngles.y * Mathf.PI / 180));
+        //Vector3 location = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z);
+        transform.position = location;
+    }
+
+    public void set_render(bool render)
+    {
+        if(render)
+        {
+            mesh.enabled = true;
+        }
+        else
+        {
+            mesh.enabled = false;
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,33 +8,64 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
     public TextMeshProUGUI countText;
+    public TextMeshProUGUI powerText;
     public GameObject winTextObject;
+    public Arrow arrow;
+
+    private float angle;
+    private float angle_rad;
+    private float angle_increase;
+    private float acceleration;
+    private float power;
+    private bool keyShoot;
 
     private Rigidbody rb;
     private int count;
     private float movementX;
     private float movementY;
+
+    private enum state
+     {
+        aiming, shooting, moving
+     };
+    private state curState;
     
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         count = 0;
-
+        curState = state.aiming;
         SetCountText();
         winTextObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if(rb.velocity == new Vector3(0.0f, 0.0f, 0.0f))
+        {
+            
+        }
     }
 
     void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
 
+        angle_increase = movementVector.x;
+        acceleration = 1.0f;
+
         movementX = movementVector.x;
         movementY = movementVector.y;
+        SetCountText();
+        if (movementY > 0)
+        {
+            keyShoot = true;
+        }
     }
 
     void SetCountText()
     {
-        countText.text = "Count:" + count.ToString();
+        countText.text = "Angle:" + angle.ToString();
         if(count >= 8)
         {
             winTextObject.SetActive(true);
@@ -43,9 +74,58 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        switch(curState)
+        {
+            case state.aiming:
+                //pretty self explanitory, angle is increased and then converted to radians
+                angle += angle_increase * acceleration;
+                angle_rad = angle * Mathf.PI / 180;
 
-        rb.AddForce(movement * speed);
+                //accelerates the turning speed
+                if (acceleration < 5.0f)
+                {
+                    acceleration += 0.1f;
+                }
+
+                //turns the ball and cursor
+                arrow.transform.rotation = Quaternion.Euler(0, angle, 0);
+                arrow.transform.rotation = Quaternion.Euler(0, angle, 0);
+                arrow.set_render(true);
+                if (keyShoot)
+                {
+                    curState = state.shooting;
+                    keyShoot = false;
+                }
+                break;
+            case state.shooting:
+                Vector3 movement = new Vector3(Mathf.Sin(angle_rad), 0.0f, Mathf.Cos(angle_rad));
+                power++;
+                if(power > 100)
+                {
+                    power = 0;
+                }
+                powerText.text = "POW:" + power.ToString();
+                if (keyShoot)
+                {
+                    rb.AddForce(movement * (power * 15));
+                    curState = state.moving;
+                    keyShoot = false;
+                }
+                break;
+            case state.moving:
+                arrow.set_render(false);
+                if(rb.velocity.y == 0.0f && keyShoot)
+                {
+                    rb.AddForce(new Vector3(0.0f, 200.0f, 0.0f));
+                    keyShoot = false;
+                }
+                if(rb.velocity == new Vector3(0.0f, 0.0f, 0.0f))
+                {
+                    curState = state.aiming;
+                    keyShoot = false;
+                }
+                break;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -57,5 +137,10 @@ public class PlayerController : MonoBehaviour
 
             SetCountText();
         }
+    }
+
+    private void hit_ball()
+    {
+        
     }
 }
